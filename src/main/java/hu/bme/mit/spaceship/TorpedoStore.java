62f4ef29a446e2c9d18d30d9053ca12cb1dd4196
LazarZsolt15 <lazarzsolti15@gmail.com>
2024-05-02 20:07:09 +0200

    review Fix #2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -37,7 +37,7 @@ public class TorpedoStore {
 
     // simulate random overheating of the launcher bay which prevents firing
     
-    double r = generator.nextDouble();
+    double r = randomNumberGenerator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
